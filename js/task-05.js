7e69794de4fb652eeb331fc68a97b0fe5ca817cd
Viacheslav Vinichenko <fortunebetting@gmail.com>
2022-09-09 15:24:59 +0300

    update 5

--- js/task-05.js
+++ js/task-05.js
@@ -2,7 +2,7 @@ const inputName = document.querySelector("#name-input");
 const nameChanger = document.querySelector("span#name-output");
 inputName.addEventListener('input', onInputChanger)
 function onInputChanger(event) {
-   console.dir (inputName)
+   
     if (inputName.value === "") {
         nameChanger.textContent = "Anonymous"
     }
