e81b030600e4ac183d9fee76569967a3c438ff42
Viacheslav Vinichenko <fortunebetting@gmail.com>
2022-09-09 21:41:03 +0300

    task - 07.js done

---
+++ js/task-07.js
@@ -0,0 +1,9 @@
+const input = document.querySelector("#font-size-control")
+console.dir(input)
+const outputText = document.querySelector('span#text')
+
+input.addEventListener("click", onChangeRange)
+function onChangeRange(event) {
+    
+   outputText.style.fontSize = `${input.value}px`
+}
