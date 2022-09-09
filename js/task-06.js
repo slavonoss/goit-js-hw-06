f23187d92c29329157033372decfffcf8bbb55c3
Viacheslav Vinichenko <fortunebetting@gmail.com>
2022-09-09 16:21:44 +0300

    task - 06 done

---
+++ js/task-06.js
@@ -0,0 +1,16 @@
+const inputEl = document.querySelector("input")
+
+const validBorder = document.querySelector('validation-input.valid')
+
+inputEl.addEventListener('input', onInputChanger);
+function onInputChanger(){
+    if (inputEl.value.length < inputEl.dataset.length) {
+       
+        inputEl.classList.add('invalid')
+        
+    }
+    else {
+        inputEl.classList.replace('invalid', 'valid' )
+     
+    }
+}
