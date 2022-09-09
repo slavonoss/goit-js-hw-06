7f75ff73ab5fda85b11b695e58c9ed0e4301f0dd
Viacheslav Vinichenko <fortunebetting@gmail.com>
2022-09-09 20:31:59 +0300

    Update task-06.js
    input to blur

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,7 @@ const inputEl = document.querySelector("input")
 
 const validBorder = document.querySelector('validation-input.valid')
 
-inputEl.addEventListener('input', onInputChanger);
+inputEl.addEventListener('blur', onInputChanger);
 function onInputChanger(){
     if (inputEl.value.length < inputEl.dataset.length) {
        
