01eea2264de10fa2ab2f4f0bd7577fc93ed06419
Viacheslav Vinichenko <fortunebetting@gmail.com>
2022-09-13 20:55:07 +0300

    task - 09 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btn = document.querySelector('button');
+const colorName = document.querySelector('span.color');
+const body = document.querySelector('body');
+btn.addEventListener('click', onChangeHexColor);
+function onChangeHexColor() {
+  const hexColor = getRandomHexColor();
+  colorName.textContent = hexColor;
+  body.style.background = hexColor;
+  btn.style.background = hexColor;
+}
