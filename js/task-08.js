9cbe6b49fb16c5d8be022540111a58442e6cb226
Viacheslav Vinichenko <fortunebetting@gmail.com>
2022-09-13 20:09:52 +0300

    task - 08 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,5 +1,15 @@
-const submitBtn = document.querySelector('button');
-submitBtn.addEventListener('submit', onSubmitBtnClick);
+const form = document.querySelector('form.login-form');
+form.addEventListener('submit', onSubmitBtnClick);
 function onSubmitBtnClick(event) {
   event.preventDefault();
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+  if (!email || !password) {
+    alert('НЕ всі поля заповнені');
+  } else {
+    const formData = { email, password };
+    console.log(formData);
+    event.currentTarget.reset();
+  }
 }
