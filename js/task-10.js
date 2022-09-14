a3c34f246468b9050c2eee4a67e773a7b35f4076
Viacheslav Vinichenko <fortunebetting@gmail.com>
2022-09-14 15:53:46 +0300

    update task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,32 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const boxes = document.querySelector('div#boxes');
+const counter = document.querySelector('input');
+const creatBtn = document.querySelector('button[data-create]');
+const destroytBtn = document.querySelector('button[data-destroy]');
+creatBtn.addEventListener('click', onCreatBtnclick);
+function onCreatBtnclick() {
+  let lastDiv;
+  const finLastDiv = el => {
+    if (el.children[el.children.length - 1]) {
+      finLastDiv(el.children[el.children.length - 1]);
+    } else {
+      lastDiv = el;
+    }
+  };
+
+  for (let i = 0; i < counter.value; i++) {
+    const container = document.createElement('div');
+    container.style.width = 30 + 10 * i + 'px';
+    container.style.height = 30 + 10 * i + 'px';
+    container.style.background = getRandomHexColor();
+    if (counter.value == 1) {
+      boxes.appendChild(container);
+      break;
+    }
+    finLastDiv(boxes);
+    lastDiv.appendChild(container);
+  }
+}
+// (boxes.children[boxes.children.length - 1]).appendChild(container);
